c3ebef306879794b9e9f990ffece8eb52ed43983
julian-borray <julian.borray@mercadolibre.com.co>
2021-11-02 18:42:14 -0500

    add strategies

--- src/classes/SubmachineStrategies/BullfrogStrategy.ts
+++ src/classes/SubmachineStrategies/BullfrogStrategy.ts
@@ -1,27 +1,93 @@
 import {IStrategy, IWeaponData} from "../../models/IStrategy";
 
 export class BullfrogStrategy implements IStrategy {
+    muzzle = true;
+    barrel = true;
+    laser = false;
+    optic = false;
+    stock = false;
+    underBarrel = true;
+    ammunition = true;
+    rearGrip = true;
+    perk = true;
     calculateStatistics(): IWeaponData{
         return {
             precision: this.calculatePrecision(),
-            damage:1,
-            range:1,
-            cadence:1,
-            mobility:1,
-            control: 1,
+            damage: this.calculateDamage(),
+            range: this.calculateRange(),
+            cadence: this.calculateCadence(),
+            mobility: this.calculateMobility(),
+            control: this.calculateControl(),
             image: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2021/06/cod-warzone-bullfrog-2356725.jpg?itok=FomBYdny',
         }
     }
     calculatePrecision(): number{
-        return 1;
+        let multiplier = 5;
+        const base = 4;
+        if(this.optic){
+            multiplier = multiplier + 3;
+        }
+        if(this.underBarrel){
+            multiplier = multiplier + 4;
+        }
+        return base * multiplier;
     }
     calculateDamage(): number{
-        return 1;
+        let multiplier = 4;
+        const base = 4;
+        if(this.barrel){
+            multiplier = multiplier + 4;
+        }
+        if(this.muzzle){
+            multiplier = multiplier + 2;
+        }
+        return base * multiplier;
     }
     calculateRange(): number{
-        return 1;
+        let multiplier = 3;
+        const base = 4;
+        if(this.barrel){
+            multiplier = multiplier + 4;
+        }
+        if(this.muzzle){
+            multiplier = multiplier + 2;
+        }
+        return base * multiplier;
     }
     calculateCadence(): number{
-        return 1;
+        let multiplier = 1;
+        const base = 2;
+        if(this.barrel){
+            multiplier = multiplier + 4;
+        }
+        if(this.ammunition){
+            multiplier = multiplier + 1;
+        }
+        if(this.muzzle){
+            multiplier = multiplier + 3;
+        }
+        return base * multiplier;
+    }
+    calculateMobility() {
+        let multiplier = 5;
+        const base = 2;
+        if(this.stock){
+            multiplier = multiplier + 7;
+        }
+        if(this.ammunition){
+            multiplier = multiplier + 3;
+        }
+        return base * multiplier;
+    }
+    calculateControl(){
+        let multiplier = 2;
+        const base = 4;
+        if(this.perk){
+            multiplier = multiplier + 4;
+        }
+        if(this.laser){
+            multiplier = multiplier + 2;
+        }
+        return base * multiplier;
     }
 }
